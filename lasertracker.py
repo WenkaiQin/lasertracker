c46857ce89e57fc364dd81f3f045132a39f07ce1
Wenkai Qin <wqin@hmc.edu>
2017-07-10 14:46:45 +0900

    added a lot of things

--- lasertracker.py
+++ lasertracker.py
@@ -4,9 +4,10 @@ import numpy as np
 thresh_min = np.array([30,70,30])
 thresh_max = np.array([70,255,255])
 
-LIVEFEED = True;
+LIVEFEED = False;
 BUFFER_SIZE = 5;
 
+# Create threshold mask based on input HSV boundaries. Input image BGR.
 def thresh_mask(src, lowerb, upperb):
 
 	im_h = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
@@ -27,9 +28,21 @@ def find_max_index(cnt):
 	return max_i
 
 
+# For mouse callback testing, remove later.
+def draw_circle(event, x, y, flags, param):
+
+	print "Callback function initiated. Event: ", event
+
+	if event == cv2.EVENT_LBUTTONDBLCLK:
+		cv2.circle(im, (x,y), 100, (255,0,0), -1)
+
+
 def main():
 
+	# Set up camera and window.
 	cap = cv2.VideoCapture(0)
+	cv2.namedWindow("Frame")
+	cv2.setMouseCallback("Frame", draw_circle)
 
 	while True:
 
@@ -37,7 +50,11 @@ def main():
 		if LIVEFEED:
 			im = cap.read()[1]
 		else:
-			im = cv2.imread("ocharef.jpg")
+			im = cv2.imread("images/ocharef.jpg")
+
+		# Make the image smaller and flip so it mirrors movement.
+		im = cv2.pyrDown(im)
+		im = cv2.flip(im, 1)
 
 		# Make mask of desired threshold.
 		mask1 = thresh_mask(im, thresh_min, thresh_max)
@@ -58,7 +75,22 @@ def main():
 			cv2.drawContours(im, [hull], 0, (0,200,0), 3)
 			# Find its moment and use it to calculate the region center.
 			M1 = cv2.moments(max_cnt)
-			cx, cy = int(M1["m10"]/M1["m00"]), int(M1["m01"]/M1["m00"])
+			try:
+				cx, cy = int(M1["m10"]/M1["m00"]), int(M1["m01"]/M1["m00"])
+				# print"#####################"
+				# print "contents of max_cnt:"
+				# print max_cnt
+				# print "value of moment:"
+				# print M1["m00"]
+				# print"#####################\n"
+			except ZeroDivisionError:
+				print"#####################"
+				print "encountered ZDE!\n"
+				print "contents of max_cnt:"
+				print max_cnt, "\n"
+				print "value of moment:"
+				print M1["m00"]
+				print"#####################\n"
 
 		else:
 			cx, cy = im.shape[:2];
@@ -72,10 +104,10 @@ def main():
 
         # Show the results.
 		cv2.imshow("Frame", im)
-		cv2.imshow("Mask", mask)
+		# cv2.imshow("Mask", mask)
 
 		if cv2.waitKey(10) == 27:
-			print "Exiting..."
+			print "\tExiting..."
 			if LIVEFEED:
 				cap.release()
 			cv2.destroyAllWindows()
---
+++ lockon.py
@@ -0,0 +1,119 @@
+import cv2
+import numpy as np
+from collections import deque
+
+color = None
+lb = np.array([0,0,0])
+ub = np.array([0,0,0])
+
+HSV_THRESHOLD = np.array([15,170,150])
+PATH_LENGTH = 10
+SHOW_ALL_DETECTIONS = True	
+
+def on_mouse(event, x, y, flags, frame):
+	# Use globals because callback functions are a pain.
+	global color, lb, ub
+
+	if event == cv2.EVENT_LBUTTONUP:
+		color = frame[y,x].tolist()
+		lb = np.array(color - HSV_THRESHOLD/2)
+		ub = np.array(color + HSV_THRESHOLD/2)
+		print 'Lower bound: ', lb
+		print 'Upper bound: ', ub
+		print
+
+
+def main():
+	# Turn on camera.
+	cap = cv2.VideoCapture(0)
+	readable, im = cap.read()
+
+	# Set up window.
+	cv2.namedWindow('Frame')
+	cv2.namedWindow('Results')
+	cv2.moveWindow('Results', 0, 400)
+
+	# Exit with error if camera couldn't turn on or read.
+	if not readable:
+		print 'ERROR: The camera could not be opened or read.'
+		print 'Exiting...'
+		return 0
+
+
+	# Initialize path queue.
+	path = deque(maxlen=PATH_LENGTH)
+
+	# Loop for each frame.
+	while True:
+		# Capture and format frame.
+		im = cap.read()[1]
+		im = cv2.pyrDown(im)
+		im = cv2.flip(im, 1)
+		im_hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+
+		# Callback if mouse action in frame.
+		cv2.setMouseCallback('Frame', on_mouse, im_hsv)
+
+		# Make mask from threshold, 3 channel version for display.
+		mask = cv2.inRange(im_hsv, lb, ub)
+		mask_3ch = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+
+		# Use mask to find and draw contour/hull.
+		results = np.copy(im)
+		contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
+
+		if len(contours)>0:
+			max_contour = max(contours, key=cv2.contourArea)
+
+			# Filter contours by size. Accomplishes morph opening without the computation!
+			contours = [contour for contour in contours if len(contour)>25]
+
+			# Make convex hulls for each contour, as well as max hull.
+			hulls = [cv2.convexHull(contour) for contour in contours]
+			max_hull = cv2.convexHull(max_contour)
+
+			# Determine center of max contour using moment.
+			max_M = cv2.moments(max_contour)
+			if max_M['m00'] == 0:
+				[cx,cy] = max_contour[0][0]
+			else:
+				cx = int(max_M['m10']/max_M['m00'])
+				cy = int(max_M['m01']/max_M['m00'])
+
+			# Append center to path.
+			path.append((cx,cy))
+
+			# Draw path.
+			for i in range(len(path)-1):
+				cv2.line(results, (path[i]), (path[i+1]), (255,170,86), 2)
+
+			# Draw the center.
+			cv2.circle(results, (cx, cy), 7, (255, 255, 255), -1)
+
+			# Draw all hulls or just the biggest (depends on SHOW_ALL_DETECTIONS).
+			if SHOW_ALL_DETECTIONS:
+				cv2.drawContours(results, hulls, -1, (0,255,255), 2)
+			cv2.drawContours(results, [max_hull], -1, (255,0,0), 2)
+
+		# Display results.
+		cv2.imshow('Results', results)
+
+		# Add mask to frame.
+		frame = np.concatenate((im,mask_3ch), axis=1)
+
+		# Add text to frame, show frame.
+		cv2.putText(frame, 'HSV: '+str(color), (30,30), 1, 1.5, (255,255,255), 2)
+		cv2.imshow('Frame', frame)
+
+		# Key commands.
+		key = cv2.waitKey(10)
+		if key == 27:
+			print 'Exiting...'
+			cap.release()
+			return 0
+		elif key == ord('h'):
+			SHOW_ALL_DETECTIONS = ~SHOW_ALL_DETECTIONS
+
+
+if __name__ == '__main__':
+	main()
